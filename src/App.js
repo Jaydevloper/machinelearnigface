5520f251869911a44becb0e53038774059be6e23
Jaydeveloper <jayhanmirx@gmail.com>
2022-08-02 14:40:24 +0500

    Ai

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import * as facemash from '@tensorflow-models/face-landmarks-detection'
+import * as tf from '@tensorflow/tfjs'
+import './App.css'
+import Webcam from 'react-webcam';
+import { useEffect, useRef } from 'react';
+import { drawMesh } from './utils';
 
 function App() {
+  const webRef = useRef()
+  const canvasRef = useRef()
+  const runFacemash = async() =>{
+    const net = await facemash.load(facemash.SupportedPackages.mediapipeFacemesh)
+    setInterval(() =>{
+      detect(net)
+    },10)
+    
+  }
+
+  const detect = async(net) => {
+    if (typeof webRef.current !== 'undefined' && webRef.current !== null && webRef.current.video.readyState ===4){
+      const video = webRef.current.video
+      const videoWidth = webRef.current.video.videoWidth
+      const videoHeight = webRef.current.video.videoHeight
+
+      webRef.current.video.widht = videoWidth
+      webRef.current.video.height = videoHeight
+
+      canvasRef.current.widht = videoWidth
+      canvasRef.current.height = videoHeight
+
+      const face = await net.estimateFaces({input: video})
+      console.log(face);
+
+      const ctx = canvasRef.current.getContext('2d')
+      requestAnimationFrame(() => {
+        drawMesh(face,ctx)
+      })
+    }
+  }
+  useEffect(()=>{
+      runFacemash()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='App'>
+    <header className='App-header'>
+      <Webcam ref={webRef} className='webcam' />
+      <canvas ref={canvasRef} className='canvas' />
+    </header>
+  </div>
   );
 }
 
